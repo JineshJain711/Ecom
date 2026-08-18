d235adc241448e08993681e85458ca33042333af
Jinesh jain <jineshjain711@gmail.com>
2026-08-19 00:45:11 +0530

    Update environment variables and enhance backend server configuration

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,24 @@ const analyticsRoutes = require('./routes/analytics.routes');
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], // Replace with your frontend URL
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', process.env.FRONTEND_URL], // Replace with your frontend URL
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 }));
 app.use(express.json());
 
 connectDB();
-
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+  app.use((req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('ShopNest API is running in Development mode...');
+  });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Backend is running on http://localhost:${PORT}`);
